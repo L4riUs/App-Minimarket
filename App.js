aa8e4ea23f3f23675b59aeb72debb5a412338383
L4riUs <garnicaluis391@gmail.com>
2024-07-27 16:01:45 -0400

    Primeros pasos

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Esta sera la app del minimarket, jajaja xD</Text>
       <StatusBar style="auto" />
     </View>
   );
