4c7269f4e256813931c80b95f0ea0e20e0148bf1
L4riUs <garnicaluis391@gmail.com>
2024-07-27 16:43:32 -0400

    Se le añadio tailwind css para los estilos

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Esta sera la app del minimarket, jajaja xD</Text>
+    <View className="flex-1 justify-center items-center bg-slate-600">
+      <Text className="text-white">Esta sera la app del minimarket, jajaja xD</Text>
+      <Image width={500} source={require('./assets/icon.png')} className="w-40 h-40"/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
